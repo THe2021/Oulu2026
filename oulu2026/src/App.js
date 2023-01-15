8a9d18b17e879668bc4c4d2ef023792d9b79a1aa
Tomi Heinonen <theinone19@student.oulu.fi>
2023-01-15 12:05:24 +0200

    toinen versio

--- oulu2026/src/App.js
+++ oulu2026/src/App.js
@@ -1,22 +1,39 @@
 import kartta from './kartta.JPG';
-import './App.css';
+import { useState } from 'react'
+
+
 
 function App() {
 
+  
+
+  const reseptit = [
+    'Kajaani: Muikkukeitto',
+    'Kuhmi: Rönttöset',
+    'Paltamo: Kapakalakeitto',
+    'Puolanka: Nauris-maitovelli',
+    'Ristijärvi: Lammaslihavelli palvatusta lihasta',
+    'Sotkamo: Nyrkkirieska',
+    'Suomussalmi: Avokukko',
+    'Vuolujoki: Imellysmarjapuuro'
+  ]
+
+  const [selected, setSelected] = useState(0)
+  const random = () => setSelected(Math.floor(Math.random() * reseptit.length))
+
+
   return (
     <div>
-        <img src={kartta} className="App-logo" alt="kartta" />
-        <p>
-          Oulu2026
+        <p><h1>
+          Oulu2026</h1>
         </p>
-        <a
-          className="App-link"
-          href="https://www.raflaamo.fi/fi/ravintola/oulu/rosso-oulu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Rosso
-        </a>
+         <button onClick={random}><img src={kartta} className="App-logo" alt="kartta" width="500px" height="400px" />
+         Click here 
+         </button>
+        <br></br>
+        <h1>{reseptit[selected]}</h1>
+        <br></br>
+        <a href="https://www.raflaamo.fi/fi/ravintola/oulu/rosso-oulu">Rosso</a>
     </div>
   );
 }
